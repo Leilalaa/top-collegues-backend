f7862f359c51bf35728e068cf8d7893fd15c1664
LeilaLaa <aaleila19@gmail.com>
2018-02-15 16:37:41 +0100

    tp services suite

--- src/main/java/dev/application/controller/CollegueController.java
+++ src/main/java/dev/application/controller/CollegueController.java
@@ -4,40 +4,40 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import dev.application.entite.Collegue;
 import dev.application.repository.CollegueRepository;
 
 
-@Controller
+@RestController
+@CrossOrigin
 public class CollegueController {
 	
 	@Autowired
 	CollegueRepository cr;
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/collegues")
-	@ResponseBody
 	public List<Collegue> listerCollegues() {
 		
 		return cr.findAll();
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, path = "/collegues")
-	@ResponseBody
 	public Collegue sauvegarder(@RequestBody Collegue collegue) {
-		
-		cr.save(collegue);
-		return cr.findByNom(collegue.getNom());
+		if(cr.findByNom(collegue.getNom()) == null){
+			cr.save(collegue);
+			return cr.findByNom(collegue.getNom());
+		}
+		return null;
 	}
 	
 	@RequestMapping(method = RequestMethod.PATCH, path="/collegues/{nom}")
-	 @ResponseBody
 	 public Collegue reaction(@PathVariable String nom, @RequestBody Map<String,String> action) {
 		
 		Collegue c = cr.findByNom(nom);
